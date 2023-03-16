e368b3b8e4b251fef8724d48d7a29846a8ba5f3d
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-03-16 11:03:06 +0100

    add Listview

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,44 +10,40 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 500,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions
-              .map((tx) => Card(
-                      child: Row(
-                    children: [
-                      Container(
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 15),
-                          decoration: BoxDecoration(
-                              border:
-                                  Border.all(color: Colors.purple, width: 2)),
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                            '\$${tx.amount}',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.purple),
-                          )),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tx.title,
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            DateFormat.yMMMd().format(tx.date),
-                            style: TextStyle(color: Colors.grey),
-                          )
-                        ],
-                      )
-                    ],
-                  )))
-              .toList(),
-        ),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+              child: Row(
+            children: [
+              Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${transactions[index].amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  )),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    transactions[index].title,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              )
+            ],
+          ));
+        },
+        itemCount: transactions.length,
       ),
     );
   }
