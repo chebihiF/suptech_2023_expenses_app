cc0822250bcf509983050284d9edcd03331f3849
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-03-23 11:34:40 +0000

    init chart

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+import 'dart:js_util';
+
+import 'package:expenses_app/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupTransactionsValues {
+    return List.generate(7, (index) {
+      final weekday = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekday.day &&
+            recentTransactions[i].date.month == weekday.month &&
+            recentTransactions[i].date.year == weekday.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+      return {'day': DateFormat.E(weekday), 'amount': totalSum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(children: []),
+    );
+  }
+}
